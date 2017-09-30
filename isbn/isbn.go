0cdfdf58bd408822f42128eb3dcd027c39eb6b37
Hank Donnay <hdonnay@gmail.com>
2017-09-30 16:01:25 -0400

    isbn: add author call

--- isbn/isbn.go
+++ isbn/isbn.go
@@ -95,37 +95,72 @@ func (a *API) Book(ctx context.Context, isbn string) (*Book, error) {
 	return &ret.Books[0], nil
 }
 
+func (a *API) Author(ctx context.Context, id string) (*Author, error) {
+	ret := apiAuthor{}
+	u, err := a.base.Parse(path.Join("author", id))
+	if err != nil {
+		return nil, err
+	}
+	b, err := a.call(ctx, u)
+	if err != nil {
+		return nil, err
+	}
+	if err := json.NewDecoder(b).Decode(&ret); err != nil {
+		return nil, err
+	}
+
+	return &ret.Authors[0], nil
+}
+
 type apiBook struct {
 	Index string `json:"index_searched"`
 	Books []Book `json:"data"`
 }
 
 type Book struct {
-	TitleLatin              string   `json:"title_latin"`
-	PhysicalDescriptionText string   `json:"physical_description_text"`
-	Summary                 string   `json:"summary"`
-	Language                string   `json:"language"`
-	SubjectIDs              []string `json:"subject_ids"`
-	Title                   string   `json:"title"`
-	ISBN10                  string   `json:"isbn10"`
-	EditionInfo             string   `json:"edition_info"`
-	DeweyDecimal            string   `json:"dewey_decimal"`
-	MarcEncLevel            string   `json:"marc_enc_level"`
-	Notes                   string   `json:"notes"`
-	AwardsText              string   `json:"awards_text"`
-	DeweyNormal             string   `json:"dewey_normal"`
-	URLsText                string   `json:"urls_text"`
-	BookID                  string   `json:"book_id"`
-	TitleLong               string   `json:"title_long"`
-	AuthorData              []Author `json:"author_data"`
-	ISBN13                  string   `json:"isbn13"`
-	PublisherText           string   `json:"publisher_text"`
-	PublisherName           string   `json:"publisher_name"`
-	LCCNumber               string   `json:"lcc_number"`
-	PublisherID             string   `json:"publisher_id"`
+	TitleLatin              string       `json:"title_latin"`
+	PhysicalDescriptionText string       `json:"physical_description_text"`
+	Summary                 string       `json:"summary"`
+	Language                string       `json:"language"`
+	SubjectID               []string     `json:"subject_ids"`
+	Title                   string       `json:"title"`
+	ISBN10                  string       `json:"isbn10"`
+	EditionInfo             string       `json:"edition_info"`
+	DeweyDecimal            string       `json:"dewey_decimal"`
+	MarcEncLevel            string       `json:"marc_enc_level"`
+	Notes                   string       `json:"notes"`
+	AwardsText              string       `json:"awards_text"`
+	DeweyNormal             string       `json:"dewey_normal"`
+	URLsText                string       `json:"urls_text"`
+	BookID                  string       `json:"book_id"`
+	TitleLong               string       `json:"title_long"`
+	AuthorData              []BookAuthor `json:"author_data"`
+	ISBN13                  string       `json:"isbn13"`
+	PublisherText           string       `json:"publisher_text"`
+	PublisherName           string       `json:"publisher_name"`
+	LCCNumber               string       `json:"lcc_number"`
+	PublisherID             string       `json:"publisher_id"`
+}
+
+type BookAuthor struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type apiAuthor struct {
+	Index   string   `json:"index_searched"`
+	Authors []Author `json:"data"`
 }
 
 type Author struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
+	AuthorID   string   `json:"author_id"`
+	CategoryID []string `json:"category_ids"`
+	Dates      string   `json:"dates"`
+	Name       string   `json:"name"`
+	SubjectID  []string `json:"subject_ids"`
+	BookCount  string   `json:"book_count"`
+	BookID     []string `json:"book_ids"`
+	LastName   string   `json:"last_name"`
+	NameLatin  string   `json:"name_latin"`
+	FirstName  string   `json:"first_name"`
 }
